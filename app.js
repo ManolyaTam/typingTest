b467ea4a074a232f90859a443f3a107834fc2401
Manolya <201097@ppu.edu.ps>
2022-07-27 00:19:49 +0300

    it works, I might need to add a couple more features, but the main goal is achieved!

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let randomTextElement = document.getElementById('randomText');
 let userInput = document.getElementById('userInput');
 let letter = document.getElementsByClassName('letter');
 let popup = document.getElementById('popup');
+let acc = document.getElementById('acc');
+let timeElement = document.getElementById('time');
+let wpm = document.getElementById('wpm');
 let randWrds =
     `Doubtful two bed way pleasure confined followed Shew up ye away no eyes life or were this Perfectly did suspicion daughters but his intention Started on society an brought it explain Position two saw greatest stronger old Pianoforte if at simplicity do estimating Up am intention on dependent questions oh elsewhere september No betrayed pleasure possible jointure we in throwing And can event rapid any shall woman green Hope they dear who its bred Smiling nothing affixed he carried it clothes calling he no
     Impossible considered invitation him men instrument saw celebrated unpleasant Put rest and must set kind next many near nayHe exquisite continued explained middleton am Voice hours young woody has she think equal
@@ -15,11 +18,14 @@ let randWrds =
 
 randWrds = randWrds.trim().replaceAll('  ', ' ');
 let randWrdsArr = randWrds.split(' ');
-addLetters = (word) => {
+addLetters = (word, firstWrd) => {
     let seperated = '';
     let size = word.length;
     for (let i = 0; i < size; i += 1) {
-        seperated += `<span class="letter">${word[i]}</span>`;
+        if (firstWrd && i === 0)
+            seperated += `<span class="letter activeLetter">${word[i]}</span>`;
+        else
+            seperated += `<span class="letter">${word[i]}</span>`;
     }
     seperated += `<span class="letter"> </span>`
     return seperated;
@@ -34,7 +40,10 @@ getRandText = () => {
     for (let j = 0; j < max_number_words; j += 1) {
         let i = Math.floor((Math.random() * 100) % (words));
         chars += randWrdsArr[i] + ' ';
-        randomText += addLetters(randWrdsArr[i]);
+        if (j === 0)
+            randomText += addLetters(randWrdsArr[i], true);
+        else
+            randomText += addLetters(randWrdsArr[i], false);
     }
     chars = chars.trim();
     randomText = randomText.trim();
@@ -44,15 +53,26 @@ getRandText = () => {
 randomTextElement.innerHTML = ``;
 randomTextElement.innerHTML = getRandText();
 
+let start;
+let end;
+let time;
+let wrong = 0;
 checkCorrect = (inputText) => {
     let i = inputText.length - 1;
+    if (i === 0) {
+        start = Date.now();
+        console.log(start);
+    }
     inputChar = inputText[i];
     if (inputChar == chars[i]) {
         letter[i].classList.add("correct");
     }
     else {
         letter[i].classList.add("wrong");
+        wrong += 1;
     }
+    letter[i].classList.remove('activeLetter');
+    letter[i + 1].classList.add('activeLetter');
     checkDone(userInput.value);
 }
 
@@ -60,12 +80,26 @@ userInput.addEventListener('keydown', (event) => {
     if (event.keyCode == 8) {
         let lastIndex = userInput.value.length - 1;
         letter[lastIndex].classList.remove("wrong", "correct");
+
+        letter[lastIndex + 1].classList.remove('activeLetter');
+        letter[lastIndex].classList.add('activeLetter');
     }
 })
 
 checkDone = (inputText) => {
     if (inputText.length === chars.length) {
+        end = Date.now()
+        time = Math.floor((end - start) / 1000);
+        minutes = time/60
         console.log('game over');
+        wpm.innerHTML = Math.floor(max_number_words/(minutes));
+        acc.innerHTML = `${Math.floor(((chars.length - wrong)/chars.length) * 100)}%` ; // wrong chars / chars.length
+        timeElement.innerHTML = (time) + 's';
         popup.style.display = 'flex';
     }
 }
+
+randomTextElement.addEventListener("click", () => { userInput.focus() })
+
+
+//
